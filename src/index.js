278bd32695fc508a5d6154932e49f9f1f7fe4fab
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-04-17 14:36:47 +0100

    Adicionar um título à página

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+document.title = 'TODO App 👍';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
